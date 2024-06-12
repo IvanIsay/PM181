8163cabe9b22b486471c972476555132caeb3dcb
lOkY <102552151+IvanIsay@users.noreply.github.com>
2024-06-11 20:40:44 -0600

    holaMundo

---
+++ ReactNative/JSReact/js/index.js
@@ -0,0 +1,30 @@
+
+console.log('Hola consola')
+/*
+// FUnciones
+
+// Tradicional
+function suma(a,b){
+    return a+b
+}
+
+//Fat Arrow
+//const resta= (a,b)=>{ return a-b }
+
+//fatArrow Simplificada
+const resta= (a,b)=> a-b 
+
+
+console.log(suma(1,10))
+console.log(resta(100,10))
+
+*/
+
+// OBJETOS
+
+const usuario={
+    nombre:'ana',
+    apellido: 'guerra',
+    edad: 30,
+    direccion:{},
+}
