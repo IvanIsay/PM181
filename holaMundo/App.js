d993df74210d1e8d4cb2f2652bf03a5db95c604a
lOkY <102552151+IvanIsay@users.noreply.github.com>
2024-06-18 20:36:43 -0600

    componetes, props States

--- holaMundo/App.js
+++ holaMundo/App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Button} from 'react-native';
+import React, {useState} from 'react';
+
+/* creamos nuestro propio componente TEXTO */
+const Texto=()=>{
+  const [ contenido,setContenido]= useState('Hola :)')
+  const actualizarTexto= ()=>{ setContenido('contenido actualizado ')}
+  return(
+    <Text onPress={actualizarTexto}> {contenido} </Text>  
+  )
+}
+
+
 
 export default function App() {
   return (
 
     <View style={styles.container}>
 
-      <Text> HOla Mundo ReactNative </Text>
+      {/* lo usamos 3 veces con etiqueta de apertura y cierre y etoqueta de autocierre */}
+      <Texto ></Texto>
+      <Texto ></Texto>
+      <Texto  />
+      
+      <Button title='Saludar' color='yellow'></Button>
+      
+    
+
       
       <StatusBar style="auto" />
 
